630cbaa2e304ca981c53f8094f6529939065d5cc
GVinodKumar <83979801+GVinodKumar@users.noreply.github.com>
2021-11-16 08:58:12 +0530

    Update Startup.cs

--- vk-vs2019-pullRequestDemo/Startup.cs
+++ vk-vs2019-pullRequestDemo/Startup.cs
@@ -33,6 +33,7 @@ namespace vk_vs2019_pullRequestDemo
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Mallikarjuna");
                 });
             });
         }
